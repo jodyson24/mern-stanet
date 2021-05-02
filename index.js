bcb6683b3c08a74638ad5d0d7b85fabdcbd04adc
jodyson24 <joshua.adegbola20@gmail.com>
2021-05-02 09:23:30 +0100

    Corrected the env production mistake in the index.js file

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ mongoose.connect(URI, {
     console.log('Connected to mongodb')
 })
 
-if(process.eventNames.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
