29819e6691093912664757512a8475bf57f9019a
jodyson24 <joshua.adegbola20@gmail.com>
2022-10-29 11:14:32 +0100

    futher changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
 import { useEffect } from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { refreshToken } from './redux/actions/authAction'
-import { getPosts } from './redux/actions/postAction'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import PageRender from './customRouter/PageRender'
 import PrivateRouter from './customRouter/PrivateRouter'
@@ -17,6 +11,10 @@ import Register from './pages/register'
 import Alert from './components/alert/Alert'
 import Header from './components/header/Header'
 import StatusModal from './components/StatusModal'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { refreshToken } from './redux/actions/authAction'
+import { getPosts } from './redux/actions/postAction'
 import { getSuggestions } from './redux/actions/suggestionsAction'
 
 import io from 'socket.io-client'
@@ -24,48 +22,50 @@ import { GLOBALTYPES } from './redux/actions/globalTypes'
 import SocketClient from './SocketClient'
 
 import { getNotifies } from './redux/actions/notifyAction'
+import CallModal from './components/message/CallModal'
+import Peer from 'peerjs'
 
 function App() {
-  const { auth, status, modal } = useSelector(state => state)
+  const { auth, status, modal, call } = useSelector(state => state)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(refreshToken())
 
     const socket = io()
-    dispatch({ type: GLOBALTYPES.SOCKET, payload: socket })
+    dispatch({type: GLOBALTYPES.SOCKET, payload: socket})
     return () => socket.close()
-
-  }, [dispatch])
+  },[dispatch])
 
   useEffect(() => {
-    if (auth.token) {
+    if(auth.token) {
       dispatch(getPosts(auth.token))
       dispatch(getSuggestions(auth.token))
       dispatch(getNotifies(auth.token))
     }
   }, [dispatch, auth.token])
 
+  
   useEffect(() => {
-    // Let's check if the browser supports notifications
     if (!("Notification" in window)) {
       alert("This browser does not support desktop notification");
     }
-
-    // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
-      // If it's okay let's create a notification
-    }
-    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission === "granted") {}
     else if (Notification.permission !== "denied") {
       Notification.requestPermission().then(function (permission) {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-
-        }
+        if (permission === "granted") {}
       });
     }
-  }, [])
+  },[])
+
+ 
+  useEffect(() => {
+    const newPeer = new Peer(undefined, {
+      path: '/', secure: true
+    })
+    
+    dispatch({ type: GLOBALTYPES.PEER, payload: newPeer })
+  },[dispatch])
 
 
   return (
@@ -78,14 +78,14 @@ function App() {
           {auth.token && <Header />}
           {status && <StatusModal />}
           {auth.token && <SocketClient />}
-
-          <Route exact path='/' component={auth.token ? Home : Login} />
+          {call && <CallModal />}
+          
+          <Route exact path="/" component={auth.token ? Home : Login} />
           <Route exact path="/register" component={Register} />
 
-          <div className="wrap_page">
-            <PrivateRouter exact path='/:page' component={PageRender} />
-            <PrivateRouter exact path='/:page/:id' component={PageRender} />
-          </div>
+          <PrivateRouter exact path="/:page" component={PageRender} />
+          <PrivateRouter exact path="/:page/:id" component={PageRender} />
+          
         </div>
       </div>
     </Router>
