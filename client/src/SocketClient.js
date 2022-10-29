7c8de19a54e66d9df0fc83566dfcd33ded6ca256
jodyson24 <joshua.adegbola20@gmail.com>
2022-10-29 11:05:13 +0100

    Updated the socket

--- client/src/SocketClient.js
+++ client/src/SocketClient.js
@@ -1,129 +1,197 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { GLOBALTYPES } from './redux/actions/globalTypes'
 import { POST_TYPES } from './redux/actions/postAction'
-// import { PROFILE_TYPES } from './redux/actions/profileAction'
+import { GLOBALTYPES } from './redux/actions/globalTypes'
 import { NOTIFY_TYPES } from './redux/actions/notifyAction'
-import audiobell from './audio/alert.wav'
+import { MESS_TYPES } from './redux/actions/messageAction'
+
+import audiobell from './audio/got-it-done-613.mp3'
 
 
 const spawnNotification = (body, icon, url, title) => {
     let options = {
         body, icon
     }
-
     let n = new Notification(title, options)
-    n.onClick = e => {
+
+    n.onclick = e => {
         e.preventDefault()
         window.open(url, '_blank')
     }
 }
 
-
-export default function SocketClient() {
-    const { auth, socket, notify } = useSelector(state => state)
-    const dispatch  = useDispatch()
+const SocketClient = () => {
+    const { auth, socket, notify, online, call } = useSelector(state => state)
+    const dispatch = useDispatch()
 
     const audioRef = useRef()
 
-    // Join User
+    // joinUser
     useEffect(() => {
-        socket.emit('joinUser', auth.user._id)
-    },[socket, auth.user._id])
-
+        socket.emit('joinUser', auth.user)
+    },[socket, auth.user])
 
     // Likes
     useEffect(() => {
-        socket.on('likeToClient', newPost => {
-            dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost})
+        socket.on('likeToClient', newPost =>{
+            dispatch({type: POST_TYPES.UPDATE_POST, payload: newPost})
         })
 
         return () => socket.off('likeToClient')
     },[socket, dispatch])
 
-    // Unlikes
     useEffect(() => {
-        socket.on('unLikeToClient', newPost => {
-            dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost})
+        socket.on('unLikeToClient', newPost =>{
+            dispatch({type: POST_TYPES.UPDATE_POST, payload: newPost})
         })
 
         return () => socket.off('unLikeToClient')
     },[socket, dispatch])
 
+
     // Comments
     useEffect(() => {
-        socket.on('createCommentToClient', newPost => {
-            dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost})
+        socket.on('createCommentToClient', newPost =>{
+            dispatch({type: POST_TYPES.UPDATE_POST, payload: newPost})
         })
 
         return () => socket.off('createCommentToClient')
     },[socket, dispatch])
 
-    // Delete Comments
     useEffect(() => {
-        socket.on('deleteCommentToClient', newPost => {
-            dispatch({ type: POST_TYPES.UPDATE_POST, payload: newPost})
+        socket.on('deleteCommentToClient', newPost =>{
+            dispatch({type: POST_TYPES.UPDATE_POST, payload: newPost})
         })
 
         return () => socket.off('deleteCommentToClient')
     },[socket, dispatch])
 
+
     // Follow
     useEffect(() => {
-        socket.on('followToClient', newUser => {
-            dispatch({ type: GLOBALTYPES.AUTH, payload: {...auth, user: newUser}})
+        socket.on('followToClient', newUser =>{
+            dispatch({type: GLOBALTYPES.AUTH, payload: {...auth, user: newUser}})
         })
 
         return () => socket.off('followToClient')
     },[socket, dispatch, auth])
 
-    // Unfollow
     useEffect(() => {
-        socket.on('unFollowToClient', newUser => {
-            dispatch({ type: GLOBALTYPES.AUTH, payload: {...auth, user: newUser}})
+        socket.on('unFollowToClient', newUser =>{
+            dispatch({type: GLOBALTYPES.AUTH, payload: {...auth, user: newUser}})
         })
 
         return () => socket.off('unFollowToClient')
     },[socket, dispatch, auth])
 
-    // CreatePost
-    // useEffect(() => {
-    //     socket.on('createPostToClient', newPost => {
-    //         dispatch({ type: POST_TYPES.CREATE_POST, payload: newPost})
-    //     })
-
-    //     return () => socket.off('createPostToClient')
-    // },[socket, dispatch])
 
     // Notification
     useEffect(() => {
-        socket.on('createNotifyToClient', msg => {
-            dispatch({ type: NOTIFY_TYPES.CREATE_NOTIFY, payload: msg})
+        socket.on('createNotifyToClient', msg =>{
+            dispatch({type: NOTIFY_TYPES.CREATE_NOTIFY, payload: msg})
+
             if(notify.sound) audioRef.current.play()
             spawnNotification(
                 msg.user.username + ' ' + msg.text,
                 msg.user.avatar,
-                "Stanet"
+                msg.url,
+                'V-NETWORK'
             )
         })
 
         return () => socket.off('createNotifyToClient')
-    },[socket, dispatch, notify])
+    },[socket, dispatch, notify.sound])
 
     useEffect(() => {
-        socket.on('removeNotifyToClient', msg => {
-            dispatch({ type: NOTIFY_TYPES.REMOVE_NOTIFY, payload: msg})
+        socket.on('removeNotifyToClient', msg =>{
+            dispatch({type: NOTIFY_TYPES.REMOVE_NOTIFY, payload: msg})
         })
 
         return () => socket.off('removeNotifyToClient')
     },[socket, dispatch])
 
 
+    // Message
+    useEffect(() => {
+        socket.on('addMessageToClient', msg =>{
+            dispatch({type: MESS_TYPES.ADD_MESSAGE, payload: msg})
+
+            dispatch({
+                type: MESS_TYPES.ADD_USER, 
+                payload: {
+                    ...msg.user, 
+                    text: msg.text, 
+                    media: msg.media
+                }
+            })
+        })
+
+        return () => socket.off('addMessageToClient')
+    },[socket, dispatch])
+
+    // Check User Online / Offline
+    useEffect(() => {
+        socket.emit('checkUserOnline', auth.user)
+    },[socket, auth.user])
+
+    useEffect(() => {
+        socket.on('checkUserOnlineToMe', data =>{
+            data.forEach(item => {
+                if(!online.includes(item.id)){
+                    dispatch({type: GLOBALTYPES.ONLINE, payload: item.id})
+                }
+            })
+        })
+
+        return () => socket.off('checkUserOnlineToMe')
+    },[socket, dispatch, online])
+
+    useEffect(() => {
+        socket.on('checkUserOnlineToClient', id =>{
+            if(!online.includes(id)){
+                dispatch({type: GLOBALTYPES.ONLINE, payload: id})
+            }
+        })
+
+        return () => socket.off('checkUserOnlineToClient')
+    },[socket, dispatch, online])
+
+    // Check User Offline
+    useEffect(() => {
+        socket.on('CheckUserOffline', id =>{
+            dispatch({type: GLOBALTYPES.OFFLINE, payload: id})
+        })
+
+        return () => socket.off('CheckUserOffline')
+    },[socket, dispatch])
+
+
+    // Call User
+    useEffect(() => {
+        socket.on('callUserToClient', data =>{
+            dispatch({type: GLOBALTYPES.CALL, payload: data})
+        })
+
+        return () => socket.off('callUserToClient')
+    },[socket, dispatch])
+
+    useEffect(() => {
+        socket.on('userBusy', data =>{
+            dispatch({type: GLOBALTYPES.ALERT, payload: {error: `${call.username} is busy!`}})
+        })
+
+        return () => socket.off('userBusy')
+    },[socket, dispatch, call])
+
+
+
     return (
         <>
-            <audio controls ref={audioRef} style={{display: 'none'}}>
-                <source src={audiobell} type="audio/wav" />
+            <audio controls ref={audioRef} style={{display: 'none'}} >
+                <source src={audiobell} type="audio/mp3" />
             </audio>
         </>
     )
 }
+
+export default SocketClient
